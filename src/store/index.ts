cbc029a9a4a2eb9d0400fa4ed4fc6aeae65967aa
tenbamboo <liu19891014@163.com>
2022-05-19 19:02:55 +0800

    优化细节

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,8 @@
 import type { App } from "vue";
 import { createPinia } from "pinia";
+import type { Pinia } from "pinia";
 
-export const store = createPinia();
+export const store: Pinia = createPinia();
 
 export function setupStore(app: App<Element>) {
   app.use(store);
